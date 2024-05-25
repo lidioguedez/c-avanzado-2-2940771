2e97b577963877e8047a81c4e10124724e4397de
Lidio Guedez <112365426+lidioguedez@users.noreply.github.com>
2024-05-25 12:40:50 -0300

    2 - 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Avanzado2_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            object miNombre = "Lidio Guedez";
+            if (miNombre is string)
+                Console.WriteLine("El nombre {0} tiene {1} caracteres",
+                    (string)miNombre, ((string)miNombre).Length);
+
+            if (miNombre is string mn)
+                Console.WriteLine("El nombre {0} tiene {1} caracteres",
+                    mn, mn.Length);
+
+        }
+    }
+}
